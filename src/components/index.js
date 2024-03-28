7d1e1ac99718766754ea7708424a56e4b4e4ded2
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-03-28 16:38:36 +0200

    ADD: orders page

--- src/components/index.js
+++ src/components/index.js
@@ -11,3 +11,4 @@ export { default as SparkLine } from "./Charts/SparkLine";
 export { default as LineChart } from "./Charts/LineChart";
 export { default as Stacked } from "./Charts/Stacked";
 export { default as Pie } from "./Charts/Pie";
+export { default as Header } from "./Header";
