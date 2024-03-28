2d455d5e7280fa4a8e3b33d1d6bc3938e6c50972
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-03-28 12:14:00 +0200

    ADD: navbar user items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import { FiShoppingCart } from "react-icons/fi";
 import { BsChatLeft } from "react-icons/bs";
@@ -7,24 +7,8 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import avatar from "../data/avatar.jpg";
+import { Cart, Chat, Notification, UserProfile } from ".";
 import { useStateContext } from "../contexts/ContextProvider";
-
-const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
-  <TooltipComponent content={title} position="BottomCenter">
-    <button
-      type="button"
-      onClick={() => customFunc()}
-      style={{ color }}
-      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
-    >
-      <span
-        style={{ background: dotColor }}
-        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
-      />
-      {icon}
-    </button>
-  </TooltipComponent>
-);
 
 const Navbar = () => {
   const {
@@ -106,13 +90,32 @@ const Navbar = () => {
           </div>
         </TooltipComponent>
 
-        {isClicked.cart && <div>Cart</div>}
-        {isClicked.chat && <div>Chat</div>}
-        {isClicked.notification && <div>Notification</div>}
-        {isClicked.userProfile && <div>UserProfile</div>}
+        {isClicked.cart && <Cart />}
+        {isClicked.chat && <Chat />}
+        {isClicked.notification && <Notification />}
+        {isClicked.userProfile && <UserProfile />}
       </div>
     </div>
   );
 };
+
+function NavButton({ title, customFunc, icon, color, dotColor }) {
+  return (
+    <TooltipComponent content={title} position="BottomCenter">
+      <button
+        type="button"
+        onClick={() => customFunc()}
+        style={{ color }}
+        className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+      >
+        <span
+          style={{ background: dotColor }}
+          className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+        />
+        {icon}
+      </button>
+    </TooltipComponent>
+  );
+}
 
 export default Navbar;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
 export { default as ThemeSettings } from "./ThemeSettings";
 export { default as Sidebar } from "./Sidebar";
 export { default as Navbar } from "./Navbar";
+export { default as Cart } from "./Cart";
+export { default as Chat } from "./Chat";
+export { default as Notification } from "./Notification";
+export { default as UserProfile } from "./UserProfile";
+export { default as Button } from "./Button";
