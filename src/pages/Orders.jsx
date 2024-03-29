671930aef3266bdd9d295e88600122b57c2ee427
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-03-29 09:52:54 +0200

    ADD: customers page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -22,7 +22,7 @@ const Orders = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
-      <div className=" w-[500px] lg:w-[650px] xl:w-[900px]">
+      <div className=" w-[500px] lg:w-[650px] xl:w-auto">
         <GridComponent
           id="gridcomp"
           dataSource={ordersData}
