e952d33ad3e9c89e81518ef5257def85b20bd778
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-03-29 10:31:30 +0200

    ADD: kanban page

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,5 +1,28 @@
-const Kanban = () => {
-  return <div>Kanban</div>;
-};
+import {
+  KanbanComponent,
+  ColumnsDirective,
+  ColumnDirective,
+} from "@syncfusion/ej2-react-kanban";
+
+import { kanbanData, kanbanGrid } from "../data/dummy";
+import { Header } from "../components";
+
+const Kanban = () => (
+  <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+    <Header title="Kanban" />
+    <KanbanComponent
+      id="kanban"
+      keyField="Status"
+      dataSource={kanbanData}
+      cardSettings={{ contentField: "Summary", headerField: "Id" }}
+    >
+      <ColumnsDirective>
+        {kanbanGrid.map((item, index) => (
+          <ColumnDirective key={index} {...item} />
+        ))}
+      </ColumnsDirective>
+    </KanbanComponent>
+  </div>
+);
 
 export default Kanban;
