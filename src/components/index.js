56c172cb6a9a051b6368c0a97a8802061c54bc7b
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-03-29 14:49:08 +0200

    ADD: charts

--- src/components/index.js
+++ src/components/index.js
@@ -11,4 +11,5 @@ export { default as SparkLine } from "./Charts/SparkLine";
 export { default as LineChart } from "./Charts/LineChart";
 export { default as Stacked } from "./Charts/Stacked";
 export { default as Pie } from "./Charts/Pie";
+export { default as ChartsHeader } from "./Charts/ChartsHeader";
 export { default as Header } from "./Header";
--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { pieChartData } from "../../data/dummy";
+import { ChartsHeader, Pie as PieChart } from "../../components";
 
-const Pie = () => {
-  return <div>Pie</div>;
-};
+const Pie = () => (
+  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+    <ChartsHeader title="Project Cost Breakdown" />
+    <div className="w-full">
+      <PieChart
+        id="chart-pie"
+        data={pieChartData}
+        legendVisiblity
+        height="full"
+      />
+    </div>
+  </div>
+);
 
 export default Pie;
